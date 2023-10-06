29255e80dcaf4007a3059f65aa7258f906b17b00
Ludmibot <144268962+Ludmibot@users.noreply.github.com>
2023-10-06 16:28:02 -0300

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -80,9 +80,9 @@ let menu = `⌜ *${wm}* ⌟
 
 *Hola ${taguser}*
 
-□ *Creador: TheZero*
-□ *Numero del creador: wa.me/59897463355*
-□ *Numero de la Creadora2: wa.me/51967647592*
+□ *Creador: ludmi*
+□ *Numero del creador: https://wa.me/5493813300744*
+□ *Numero de la Creadora2:https://wa.me/5493813300744*
 □ *Tiempos activos :* *${uptime}*
 □ *Usuarios:* *${Object.keys(global.db.data.users).length}*
 □ *Registrado: ${rtotalreg} de ${totalreg}* ${(conn.user.jid == global.conn.user.jid ? '' : `\n□ *Soy un sub bot del:* wa.me/${global.conn.user.jid.split`@`[0]}`) || ''}
